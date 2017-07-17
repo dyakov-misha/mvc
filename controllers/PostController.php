12e98d90ae5d1eb78d594c0c0c5315c6b171715c
Dyakov Misha <dyakov@skobeeff.ru>
2017-07-17 16:40:13 +0300

    base commit

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -4,14 +4,22 @@ namespace controllers;
 use models\Mysql;
 use controllers\BaseController;
 
+
+
 class PostController extends BaseController
 {
-	
-
-	function request() 
+	public function actionList() 
 	{
 		$data = $this->mysql->select("SELECT * FROM article");
 		echo $this->render('views',[
+			'data' =>$data
+			]);
+	}
+	public function actionShow($id)
+	{
+		$data = $this->mysql->select("SELECT * FROM article WHERE `id`=$id");
+		$data = $data[0];
+		echo $this->render('show', [
 			'data' =>$data
 			]);
 	}
